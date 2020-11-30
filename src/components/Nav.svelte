75897a7570a9b9b7f2a8d13dee637c44617db72b
Ben Spurlock <spurlock114@gmail.com>
2020-11-29 19:12:31 -0700

    added github repo

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,62 +1,75 @@
 <script>
-  import {
-    Button,
-    Nav,
-    Navbar,
-    NavItem,
-    NavLink,
-    NavbarBrand,
-    NavbarToggler,
-    Collapse,
-    UncontrolledDropdown,
-    DropdownToggle,
-    DropdownMenu,
-    DropdownItem
-  } from "sveltestrap";
+    import {
+        Button,
+        Nav,
+        Navbar,
+        NavItem,
+        NavLink,
+        NavbarBrand,
+        NavbarToggler,
+        Collapse,
+        UncontrolledDropdown,
+        DropdownToggle,
+        DropdownMenu,
+        DropdownItem,
+    } from "sveltestrap";
+    import FaGithub from "svelte-icons/fa/FaGithub.svelte";
 
-  let isOpen = false;
+    let isOpen = false;
 
-  function handleUpdate(event) {
-    isOpen = event.detail.isOpen;
-  }
+    function handleUpdate(event) {
+        isOpen = event.detail.isOpen;
+    }
 </script>
 
 <style>
-
+    :global(.icon) {
+        width: 32px;
+        height: 32px;
+    }
 </style>
 
 <Navbar color="dark" dark expand="md">
-  <NavbarBrand rel="prefetch" href="/">Cooking with QE</NavbarBrand>
-  <NavbarToggler on:click={() => (isOpen = !isOpen)} />
-  <Collapse {isOpen} navbar expand="md" on:update={handleUpdate}>
-    <Nav class="ml-auto" navbar>
-      <NavItem>
-        <NavLink rel="prefetch" href="authors">Authors</NavLink>
-      </NavItem>
-      <!-- <NavItem>
+    <NavbarBrand rel="prefetch" href="/">Cooking with QE</NavbarBrand>
+    <NavbarToggler on:click={() => (isOpen = !isOpen)} />
+    <Collapse {isOpen} navbar expand="md" on:update={handleUpdate}>
+        <Nav class="ml-auto" navbar>
+            <NavItem>
+                <NavLink
+                    rel="prefetch"
+                    target="_blank"
+                    href="https://github.com/BenDaSpur/cwqe"
+                    class="icon">
+                    <FaGithub />
+                </NavLink>
+            </NavItem>
+            <NavItem>
+                <NavLink rel="prefetch" href="authors">Authors</NavLink>
+            </NavItem>
+            <!-- <NavItem>
         <NavLink href="/recipes">Recipes</NavLink>
       </NavItem> -->
-      <UncontrolledDropdown nav inNavbar>
-        <DropdownToggle nav caret>Recipes</DropdownToggle>
-        <DropdownMenu right>
-          <DropdownItem
-            class="text-dark"
-            onclick="location.href='/recipes?category=technical'">
-            Technical
-          </DropdownItem>
-          <DropdownItem
-            class="text-dark"
-            onclick="location.href='/recipes?category=food'">
-            Food
-          </DropdownItem>
-          <DropdownItem divider />
-          <DropdownItem
-            class="text-dark"
-            onclick="location.href='/recipes?category=all'">
-            All
-          </DropdownItem>
-        </DropdownMenu>
-      </UncontrolledDropdown>
-    </Nav>
-  </Collapse>
+            <UncontrolledDropdown nav inNavbar>
+                <DropdownToggle nav caret>Recipes</DropdownToggle>
+                <DropdownMenu right>
+                    <DropdownItem
+                        class="text-dark"
+                        onclick="location.href='/recipes?category=technical'">
+                        Technical
+                    </DropdownItem>
+                    <DropdownItem
+                        class="text-dark"
+                        onclick="location.href='/recipes?category=food'">
+                        Food
+                    </DropdownItem>
+                    <DropdownItem divider />
+                    <DropdownItem
+                        class="text-dark"
+                        onclick="location.href='/recipes?category=all'">
+                        All
+                    </DropdownItem>
+                </DropdownMenu>
+            </UncontrolledDropdown>
+        </Nav>
+    </Collapse>
 </Navbar>
--- src/components/Share.svelte
+++ src/components/Share.svelte
@@ -1,27 +1,27 @@
 <script>
-  import {
-    Email,
-    HackerNews,
-    Reddit,
-    LinkedIn,
-    Pinterest,
-    Telegram,
-    Tumblr,
-    Vk,
-    WhatsApp,
-    Xing,
-    Facebook,
-    Twitter,
-  } from "svelte-share-buttons-component";
-  export let url, title, desc;
+    import {
+        Email,
+        HackerNews,
+        Reddit,
+        LinkedIn,
+        Pinterest,
+        Telegram,
+        Tumblr,
+        Vk,
+        WhatsApp,
+        Xing,
+        Facebook,
+        Twitter,
+    } from "svelte-share-buttons-component";
+    export let url, title, desc;
 </script>
 
 <style>
-  @media only screen and (min-width: 751px) {
-    :global(.ssbc-button__link) {
-      float: right;
+    @media only screen and (min-width: 751px) {
+        :global(.ssbc-button__link) {
+            float: right;
+        }
     }
-  }
 </style>
 
 <Email subject={title} body="{desc} {url}" />
@@ -29,10 +29,10 @@
 <Reddit class="share-button" {title} {url} />
 <LinkedIn class="share-button" {url} />
 <!-- <Tumblr class="share-button" {title} {url} caption={title} /> -->
-<Pinterest class="share-button" {url} description={title} />
+<!-- <Pinterest class="share-button" {url} description={title} /> -->
 <!-- <Telegram class="share-button" text={title} {url} /> -->
 <!-- <Vk class="share-button" {title} {url} /> -->
-<WhatsApp class="share-button" text="{title} {url}" />
+<!-- <WhatsApp class="share-button" text="{title} {url}" /> -->
 <!-- <Xing class="share-button" {title} {url} /> -->
 <Facebook class="share-button" {url} />
 <Twitter class="share-button" text={title} {url} />
